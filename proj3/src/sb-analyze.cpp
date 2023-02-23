fcd3c579e349802ccf79ebfd39b668fbace86a49
Scott Joseph Emrich <semrich@tesla1.eecs.utk.edu>
2023-02-23 16:18:53 -0500

    adding files for lab3

---
+++ proj3/src/sb-analyze.cpp
@@ -0,0 +1,13 @@
+#include "disjoint_set.hpp"
+#include <iostream>
+using namespace std;
+using plank::Disjoint_Set;
+
+int main()
+{
+  Disjoint_Set d;
+
+  (void) d;
+  cout << "This program doesn't do anything yet.\n";
+  return 0;
+}
---
+++ proj3/src/sb-play.cpp
@@ -0,0 +1,9 @@
+#include "disjoint_set.hpp"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "This program doesn't do anything yet.\n";
+  return 0;
+}
